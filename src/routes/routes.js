bf05780dbdf3c14885e2d7eca4d6da31a5dd7856
nkhrystiuk <elhe2013@gmail.com>
2022-08-19 17:54:01 +0300

    fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -46,167 +46,168 @@ if (process.env.NODE_ENV === "production") {
   router.use(cors("*"));
 }
 
+
 // Routes Not checked JWT
 
 // Register
-/**
- * @swagger
- *
- * /user/register:
- *   post:
- *     tags:
- *       - Register
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *              - name
- *              - email
- *              - password *              
- *              - city
- *              - region
- *              - mobile_phone
- *             properties:
- *               name:
- *                 type: string
- *               email:
- *                 type: string
- *               password:
- *                 type: string
- *               city: 
- *                 type: String,
- *               region: 
- *                 type: String,
- *               mobile_phone: 
- *                 type: String,
- *     responses:
- *       200:
- *         description: Return json with User data create
- *         content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *                  example: true
- *                message:
- *                  type: string
- *                  example: "Successfully created new user and his Finance Data. You can Login"
- *       400:
- *         description: If not correct data request
- *         content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                success:
- *                  type: boolean
- *                  example: false
- *                message:
- *                  type: string
- *                  example: "some error written here"
- */
+// /**
+//  * @swagger
+//  *
+//  * /user/register:
+//  *   post:
+//  *     tags:
+//  *       - Register
+//  *     requestBody:
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             required:
+//  *              - name
+//  *              - email
+//  *              - password *              
+//  *              - city
+//  *              - region
+//  *              - mobile_phone
+//  *             properties:
+//  *               name:
+//  *                 type: string
+//  *               email:
+//  *                 type: string
+//  *               password:
+//  *                 type: string
+//  *               city: 
+//  *                 type: String,
+//  *               region: 
+//  *                 type: String,
+//  *               mobile_phone: 
+//  *                 type: String,
+//  *     responses:
+//  *       200:
+//  *         description: Return json with User data create
+//  *         content:
+//  *          application/json:
+//  *            schema:
+//  *              type: object
+//  *              properties:
+//  *                success:
+//  *                  type: boolean
+//  *                  example: true
+//  *                message:
+//  *                  type: string
+//  *                  example: "Successfully created new user and his Finance Data. You can Login"
+//  *       400:
+//  *         description: If not correct data request
+//  *         content:
+//  *          application/json:
+//  *            schema:
+//  *              type: object
+//  *              properties:
+//  *                success:
+//  *                  type: boolean
+//  *                  example: false
+//  *                message:
+//  *                  type: string
+//  *                  example: "some error written here"
+//  */
 router.post("user/register", UserController.userRegister);
 
-// Login
-/**
- * @swagger
- *
- * /user/login:
- *   post:
- *     tags:
- *       - Login
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *              - email
- *              - password
- *             properties:
- *               email:
- *                  type: string
- *                  example: "user@user.com"
- *               password:
- *                  type: string
- *                  example: "userPassword"
- *     responses:
- *       200:
- *         description: Return json with User data create
- *         content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                user:
- *                  type: object
- *                  example: {"id": "5c9962d4dee9ba402c2a86f9","email": "test@test.test","name": "Test Name"}
- *                token:
- *                 type: string
- *                 example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjOTk2MmQ0ZGVlOWJhNDAyYzJhODZmOSIsImVtYWlsIjoiM3YyaWt0bzN3d3I0QHRlc3R0a2hpcy5jb20iLCJuYW1lIjoiVGVzdCBTdXBlIiwiaWF0IjoxNTUzNTU2MzA2LCJleHAiOjE1NTM1NjYzMDZ9.I2V0TAlpJQdLz0x03gpfJpEPhR17MBvIyFzI3WuVXY4"
- *       400:
- *         description: If not correct data request
- *         content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  example: "Incorrect email or password."
- *                user:
- *                  type: boolean
- *                  example: false
- */
+// // Login
+// /**
+//  * @swagger
+//  *
+//  * /user/login:
+//  *   post:
+//  *     tags:
+//  *       - Login
+//  *     requestBody:
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             required:
+//  *              - email
+//  *              - password
+//  *             properties:
+//  *               email:
+//  *                  type: string
+//  *                  example: "user@user.com"
+//  *               password:
+//  *                  type: string
+//  *                  example: "userPassword"
+//  *     responses:
+//  *       200:
+//  *         description: Return json with User data create
+//  *         content:
+//  *          application/json:
+//  *            schema:
+//  *              type: object
+//  *              properties:
+//  *                user:
+//  *                  type: object
+//  *                  example: {"id": "5c9962d4dee9ba402c2a86f9","email": "test@test.test","name": "Test Name"}
+//  *                token:
+//  *                 type: string
+//  *                 example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjOTk2MmQ0ZGVlOWJhNDAyYzJhODZmOSIsImVtYWlsIjoiM3YyaWt0bzN3d3I0QHRlc3R0a2hpcy5jb20iLCJuYW1lIjoiVGVzdCBTdXBlIiwiaWF0IjoxNTUzNTU2MzA2LCJleHAiOjE1NTM1NjYzMDZ9.I2V0TAlpJQdLz0x03gpfJpEPhR17MBvIyFzI3WuVXY4"
+//  *       400:
+//  *         description: If not correct data request
+//  *         content:
+//  *          application/json:
+//  *            schema:
+//  *              type: object
+//  *              properties:
+//  *                message:
+//  *                  example: "Incorrect email or password."
+//  *                user:
+//  *                  type: boolean
+//  *                  example: false
+//  */
 router.post("user/login", UserController.userLogin);
 
 // Routes Must have checked function of JWT exp
-/**
- * @swagger
- *
- * /api/logout:
- *    get:
- *     security:
- *        type: http
- *        scheme: bearer
- *        bearerFormat: JWT
- *     parameters:
- *            - in: header
- *              name: Authorization
- *              required: true
- *              schema:
- *                type: string
- *                example: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjOTk2MmQ0ZGVlOWJhNDAyYzJhODZmOSIsImVtYWlsIjoiM3YyaWt0bzN3d3I0QHRlc3R0a2hpcy5jb20iLCJuYW1lIjoiVGVzdCBTdXBlIiwiaWF0IjoxNTUzNTU3NzI0LCJleHAiOjE1NTM1Njc3MjR9.Yuqy_d1NheW5osTAdzjSUrgAurZtXIZMjQnpTTufzhs"
- *              description: When you login write token to localStorage. Example - Bearer eyJhbGciOiJIUzI1N...
- *     tags:
- *       - Protected Routes
- *     responses:
- *       200:
- *         description: Return json with User data create
- *         content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  type: object
- *                  example: "User successfully logout"
- *       400:
- *         description: If not correct data request
- *         content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  example: "Incorrect email or password."
- *                user:
- *                  type: boolean
- *                  example: false
- */
+// /**
+//  * @swagger
+//  *
+//  * /api/logout:
+//  *    get:
+//  *     security:
+//  *        type: http
+//  *        scheme: bearer
+//  *        bearerFormat: JWT
+//  *     parameters:
+//  *            - in: header
+//  *              name: Authorization
+//  *              required: true
+//  *              schema:
+//  *                type: string
+//  *                example: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjOTk2MmQ0ZGVlOWJhNDAyYzJhODZmOSIsImVtYWlsIjoiM3YyaWt0bzN3d3I0QHRlc3R0a2hpcy5jb20iLCJuYW1lIjoiVGVzdCBTdXBlIiwiaWF0IjoxNTUzNTU3NzI0LCJleHAiOjE1NTM1Njc3MjR9.Yuqy_d1NheW5osTAdzjSUrgAurZtXIZMjQnpTTufzhs"
+//  *              description: When you login write token to localStorage. Example - Bearer eyJhbGciOiJIUzI1N...
+//  *     tags:
+//  *       - Protected Routes
+//  *     responses:
+//  *       200:
+//  *         description: Return json with User data create
+//  *         content:
+//  *          application/json:
+//  *            schema:
+//  *              type: object
+//  *              properties:
+//  *                message:
+//  *                  type: object
+//  *                  example: "User successfully logout"
+//  *       400:
+//  *         description: If not correct data request
+//  *         content:
+//  *          application/json:
+//  *            schema:
+//  *              type: object
+//  *              properties:
+//  *                message:
+//  *                  example: "Incorrect email or password."
+//  *                user:
+//  *                  type: boolean
+//  *                  example: false
+//  */
 // router.get("/logout", UserController.userLogout);
 // router.put("/change", passportCheck, UserController.userChangePassword);
 // router.get("/user", passportCheck, UserController.getUser);
@@ -272,8 +273,8 @@ router.post("user/login", UserController.userLogin);
 //  *                  type: boolean
 //  *                  example: false
 //  */
-router.get("/news", passportCheck, NewsController.getNews);
-router.get("/sponsors-info", passportCheck, SponsorsController.getSponsorsInfo);
+router.get("/news", NewsController.getNews);
+router.get("/sponsors-info", SponsorsController.getSponsorsInfo);
 // /**
 //  * @swagger
 //  *
