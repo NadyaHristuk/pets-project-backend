fede5b8003afa4bd2481601c7b2120963fac3e89
nkhrystiuk <elhe2013@gmail.com>
2022-09-18 16:26:56 +0300

    fix

--- routes/notice.js
+++ routes/notice.js
@@ -7,27 +7,23 @@ const { authMiddleware } = require("../middlewares/authMiddleware");
 const uploadCloud = require("../middlewares/uploadMiddleware.js");
 
 router.post(
-  "/notice/",
+  "/",
   authMiddleware,
   uploadCloud.single("animals_photos"),
   NoticeController.noticeCreate
 );
-router.delete("/notice/:id", authMiddleware, NoticeController.noticeDelete);
-router.get("/notice/:limit", authMiddleware, NoticeController.noticeOfUser);
-router.get("/notice/:id", NoticeController.noticeByID);
+router.delete("/:id", authMiddleware, NoticeController.noticeDelete);
+router.get("/:limit", authMiddleware, NoticeController.noticeOfUser);
+router.get("/:id", NoticeController.noticeByID);
 
-router.get(
-  "/notice/selected/",
-  authMiddleware,
-  NoticeController.noticeSelected
-);
+router.get("/selected/", authMiddleware, NoticeController.noticeSelected);
 router.post(
-  "/notice/selected/:id",
+  "/selected/:id",
   authMiddleware,
   NoticeController.noticeSelectedCreate
 );
 router.delete(
-  "/notice/selected/:id",
+  "/selected/:id",
   authMiddleware,
   NoticeController.noticeSelectedDelete
 );
--- routes/pet.js
+++ routes/pet.js
@@ -6,11 +6,11 @@ const PetController = require("../controllers/pet.js");
 const uploadCloud = require("../middlewares/uploadMiddleware.js");
 
 router.post(
-  "/pet/",
+  "/",
   authMiddleware,
   uploadCloud.single("avatarURL"),
   PetController.petRegister
 );
-router.delete("/pet/:id", authMiddleware, PetController.petDelete);
+router.delete("/:id", authMiddleware, PetController.petDelete);
 
 module.exports = router;
